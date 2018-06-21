c32c53dff7f95d7ad92544c76a4648ab6c266eef
slowmike <aznwizard123@gmail.com>
2018-06-21 12:19:53 -0700

    optimized to fully skip column and diagonal conflicts

--- src/solvers.js
+++ src/solvers.js
@@ -30,45 +30,58 @@ window.findNRooksSolution = function(n) {
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = 0;
-  // create new board sized n,n
-  var board = new Board({n: n});
-  // create a counter var for number of rooks placed
-  var rookCount = 0;
-  // create new function that takes in currentRow, board??
-  var repeat = function(currentRow, boardState) {
-    // if currentRow is equal to n
-    if (currentRow >= n) {
-      // if currentRow is equal to number of rooks
-      if (currentRow === rookCount) {
-        // solutionCount++;
-        solutionCount++;
-      }
-      // return
-      return;
-    }
+  if (n === 0) { return 1 }
+  return n * countNRooksSolutions(n - 1)
+  // var solutionCount = 0;
+  // // create new board sized n,n
+  // var board = new Board({n: n});
+  // // create a counter var for number of rooks placed
+  // var rookCount = 0;
+  // var cols = [];
+  // // create new function that takes in currentRow, board??
+  // var repeat = function(currentRow, boardState) {
+  //   // if currentRow is equal to n
+  //   if (currentRow >= n) {
+  //     // if currentRow is equal to number of rooks
+  //     if (currentRow === rookCount) {
+  //       // solutionCount++;
+  //       solutionCount++;
+  //       //cols = {};
+  //     }
+  //     // return
+  //     return;
+  //   }
 
-    // iterate through the row
-    for (var i = 0; i < n; i++) {
-      // toggle and add one to rook counter
-      boardState.togglePiece(currentRow, i);
-      rookCount++;
-      // if there is no conflict
-      if (!boardState.hasColConflictAt(i) && !boardState.hasRowConflictAt(currentRow)) {
-        console.log(JSON.stringify(boardState));
-        // rune the function with nextrow, board
-        repeat(currentRow+1, boardState);
-      }
-      // toggle and minus one to rook counter
-      boardState.togglePiece(currentRow, i);
-      rookCount--;
-    }
-  }
-  repeat(0, board);
+  //   // iterate through the row
+  //   for (var i = 0; i < n; i++) {
+  //     if (cols[i]) {
+  //       continue;
+  //     }
+  //     // toggle and add one to rook counter
+  //     boardState.togglePiece(currentRow, i);
+  //     rookCount++;
+  //     //cols[i] = true
+  //     // if there is no conflict
+  //     if (!boardState.hasColConflictAt(i) && !boardState.hasRowConflictAt(currentRow)) {
+  //       // run the function with nextrow, board
+  //       cols[i] = true;
+  //       // console.log("toggle on: "+JSON.stringify(cols));
+  //       repeat(currentRow+1, boardState);
+  //       //col = {}
+  //     }
+  //     // toggle and minus one to rook counter
+  //     boardState.togglePiece(currentRow, i);
+  //     rookCount--;
+  //     cols[i] = false;
+  //     // console.log("toggle off: "+JSON.stringify(cols));
+  //   }
+  // }
+  // repeat(0, board);
 
-  console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
-  return solutionCount;
+  // console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
+  // return solutionCount;
 };
+
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
@@ -125,6 +138,10 @@ window.countNQueensSolutions = function(n) {
   var board = new Board({n: n});
   // create a counter var for number of rooks placed
   var queenCount = 0;
+  var cols = {};
+  var majorDiag = {};
+  var minorDiag = {};
+
   // create new function that takes in currentRow, board??
   var repeat = function(currentRow, boardState) {
     // if currentRow is equal to n
@@ -140,18 +157,28 @@ window.countNQueensSolutions = function(n) {
 
     // iterate through the row
     for (var i = 0; i < n; i++) {
+      var majorI = boardState._getFirstRowColumnIndexForMajorDiagonalOn(currentRow, i);
+      var minorI = boardState._getFirstRowColumnIndexForMinorDiagonalOn(currentRow, i);
+      if (cols[i] || majorDiag[majorI] || minorDiag[minorI]) {
+        continue;
+      }
       // toggle and add one to rook counter
       boardState.togglePiece(currentRow, i);
       queenCount++;
+      cols[i] = true;
+      majorDiag[majorI] = true;
+      minorDiag[minorI] = true;
       // if there is no conflict
       if (!boardState.hasAnyQueenConflictsOn(currentRow, i)) {
-        console.log(JSON.stringify(boardState));
         // rune the function with nextrow, board
         repeat(currentRow+1, boardState);
       }
       // toggle and minus one to rook counter
       boardState.togglePiece(currentRow, i);
       queenCount--;
+      cols[i] = false;
+      majorDiag[majorI] = false;
+      minorDiag[minorI] = false;
     }
   }
   repeat(0, board);
