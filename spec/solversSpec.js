a7489b148ff70d29279cad1d3e14b5274dd2e057
slowmike <aznwizard123@gmail.com>
2018-06-21 16:05:38 -0700

    changes to solversSpec

--- spec/solversSpec.js
+++ spec/solversSpec.js
@@ -68,7 +68,7 @@ describe('solvers', function() {
   describe('countNQueensSolutions()', function() {
     var n = 8;
     it(`finds the number of valid solutions for n of 0-${n}`, function() {
-      _.range(0, n+1).map(function(n) {
+      _.range(0, n + 1).map(function(n) {
         var solutionCount = countNQueensSolutions(n);
         var expectedSolutionCount = [1, 1, 0, 0, 2, 10, 4, 40, 92, 352, 724, 2680, 14200, 73712, 365596, 2279184][n];
 
